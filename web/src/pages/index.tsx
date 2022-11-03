3c5cc6a6306a81c0c14be255f1b7b10af43b6be9
Allan Gomes <allanhouse2011@gmail.com>
2022-11-03 17:27:37 -0300

    created pools

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,16 +3,43 @@ import appPreviewImg from '../assets/copa-preview.png'
 import logoImg from '../assets/logo.svg'
 import usersAvataresExample from '../assets/users-avatares-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
+import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
-export default function Home() {
+interface HomeProps {
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
+}
+
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent ) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+
+      const { code } = response.data
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso, o código foi copiado para a area de transferencia!')
+
+      setPoolTitle('')
+
+    } catch (err) {
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
       <main>
-        <Image 
-          src={logoImg} 
-          alt="Logo Copa"
-          quality={100}
-        />
+        <Image src={logoImg} alt="Logo Copa" />
 
         <h1  className="mt-14 text-white text-5xl font-bold leading-tight">
           Crie seu próprio bolão da copa e compartilhe entre amigos!
@@ -21,16 +48,18 @@ export default function Home() {
         <div className="mt-10 flex items-center gap-2">
           <Image src={usersAvataresExample} alt="avatares" quality={100}/>
           <strong className="text-gray-100 text-xl">
-            <span className="text-ignite-500">+12.592</span> pessoas já estão usando
+            <span className="text-ignite-500">+{props.userCount}</span> pessoas já estão usando
           </strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input
-            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text"
             required
             placeholder='Qual nome do seu bolão?'
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button
             className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
@@ -48,7 +77,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="icone" quality={100}/>
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.834</span>
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
@@ -58,7 +87,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="icone" quality={100} />
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+192.847</span>
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
               <span>Palpites enviados</span>
             </div>
           </div>
@@ -75,13 +104,18 @@ export default function Home() {
   )
 }
 
-//export const getServerSideProps = async () => {
-//  const response = await fetch('http://localhost:3333/pools/count')
-//  const data = await response.json()
-//
-// return {
-//    props: {
-//      count: data.count,
-//    }
-//  }
-//}
+export const getServerSideProps = async () => {
+  const [ PoolCountResponse, GuessCountResponse, UserCountResponse ] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count')
+  ])
+
+ return {
+    props: {
+      poolCount: PoolCountResponse.data.count,
+      guessCount: GuessCountResponse.data.count,
+      userCount: UserCountResponse.data.count,
+    }
+  }
+}
