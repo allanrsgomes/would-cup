8518d1375f38bc5ce7e62a17cf9210de147285ca
Allan Gomes <allanhouse2011@gmail.com>
2022-11-04 22:04:38 -0300

    customized button login app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Signin } from './src/screens/signin';
+import { Signin } from './src/screens/Signin';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
--- mobile/src/screens/Signin.tsx
+++ mobile/src/screens/Signin.tsx
@@ -1,11 +1,20 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from 'native-base';
+import { Fontisto } from '@expo/vector-icons'
+import { Button } from '../components/Button';
+//import Logo from '../assets/logo.svg' Ajustar o LOGO depois 
+
 
 export function Signin() {
     return (
-        <Center flex={1} bgColor={'black'}>
-            <Text color="white" fontSize={24}>
-                Signin
-            </Text>
+        <Center flex={1} bgColor="gray.900">
+            {/*<Logo width={212} height={212} /> */}
+
+            <Button 
+                title="ENTRAR COM GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                type="SECONDARY"
+            />
+
         </Center>
     )
 }
